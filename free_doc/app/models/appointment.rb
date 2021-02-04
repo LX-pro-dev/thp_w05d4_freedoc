a7d641a3c1153e80e845d69f2fee67ea159071c0
LX-pro-dev <lx.pro.dev@gmail.com>
2021-02-04 13:41:51 +0100

    2.1.3 ok ; )

--- free_doc/app/models/appointment.rb
+++ free_doc/app/models/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment < ApplicationRecord
-  t.belongs_to :doctor, index: true
-  t.belongs_to :patient, index: true
+  belongs_to :doctor
+  belongs_to :patient
 
 end
--- free_doc/db/seeds.rb
+++ free_doc/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  doc = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip_code, )
+  10.times do
+    pat = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+    appoint = Appointment.create!(doctor: doc, patient: pat)
+  end
+end
