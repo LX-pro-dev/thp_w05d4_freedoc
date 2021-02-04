f81083ef275629e07b33715ac39332bb0e7321a1
LX-pro-dev <lx.pro.dev@gmail.com>
2021-02-04 16:05:27 +0100

    2.1.4 ajout de specialties et de doctor_specialties table de jointure, todo: seeds)

--- free_doc/app/models/doctor.rb
+++ free_doc/app/models/doctor.rb
@@ -2,4 +2,6 @@ class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
   has_many :doctors, through: :appointments
+  has_many :doctor_specialties
+  has_many :specialties, through: :doctor_specialties
 end
--- free_doc/db/schema.rb
+++ free_doc/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_144153) do
+ActiveRecord::Schema.define(version: 2021_02_04_145516) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2021_02_04_144153) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "doctor_specialties", force: :cascade do |t|
+    t.integer "doctor_id"
+    t.integer "specialty_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["doctor_id"], name: "index_doctor_specialties_on_doctor_id"
+    t.index ["specialty_id"], name: "index_doctor_specialties_on_specialty_id"
+  end
+
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -41,6 +50,12 @@ ActiveRecord::Schema.define(version: 2021_02_04_144153) do
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "specialties", force: :cascade do |t|
+    t.string "specialty_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
